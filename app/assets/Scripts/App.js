2598558667741f228520618bd0941d0a9bd30df0
Divya <divyavarun85@gmail.com>
2020-07-07 23:27:14 -0400

    completed modal overlay

--- app/assets/Scripts/App.js
+++ app/assets/Scripts/App.js
@@ -3,13 +3,27 @@ import MobileMenu from './Modules/MobileMenu'
 import RevealOnScroll from './Modules/RevealOnScroll'
 import StickyHeader from './Modules/stickyHeader'
 
-
-let stickyHeader = new StickyHeader()
+new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"),75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"),60)
 
 
-let mobileMenu = new MobileMenu();
+new MobileMenu();
+let modal
+
+document.querySelectorAll(".open-modal").forEach(el =>{
+    el.addEventListener("click", e => {
+        e.preventDefault()
+       if(typeof modal == "undefined"){
+            import('./modules/Modal').then(x =>{
+                modal = new x.default()
+                setTimeout(() =>modal.openTheModal(),20) 
+            }).catch(()=> console.log("There was a problem"))
+       }else{
+            modal.openTheModal()
+       }
+    })
+})
 
 if(module.hot){
     module.hot.accept()
