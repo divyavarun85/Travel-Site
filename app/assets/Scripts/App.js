ee45d2e54c462047d2b9cc4bf47bb8a8af0b17db
Divya <divyavarun85@gmail.com>
2020-07-08 21:51:54 -0400

    completed our build process configaration

--- app/assets/Scripts/App.js
+++ app/assets/Scripts/App.js
@@ -9,6 +9,7 @@ new RevealOnScroll(document.querySelectorAll(".feature-item"),75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"),60)
 
 
+
 new MobileMenu();
 let modal
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
+const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
+const {CleanWebpackPlugin} =  require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require ('html-webpack-plugin')
+const fse = require('fs-extra')
+
 const postCSSPlugins = [
     require('postcss-import'),
     require('postcss-mixins'),
@@ -9,17 +15,47 @@ const postCSSPlugins = [
     
 ]
 
-module.exports = {
+class RunAfterCompile {
+    apply(compiler){
+        compiler.hooks.done.tap('copy images', function(){
+            fse.copySync('./app/assets/images', './docs/assets/images')
+        })
+    }
+}
 
-    /** Setting the start point */
-    entry: './app/assets/scripts/App.js',
-    /**Creating a new bundled file name bundled.js in the given directory path(we delete the dist folder and create
-     * the new file in app folder) */
-    output:{
+let cssConfig = {
+    test:/\.css$/i,
+    use:['css-loader?url=false', {loader:'postcss-loader',options:{plugins:postCSSPlugins}}]
+}
+
+let pages = fse.readdirSync('./app').filter(function(file){
+    return file.endsWith('.html')
+}).map(function(page){
+    return new HtmlWebpackPlugin({
+        filename : page,
+        template:`./app/${page}`
+    })
+})
+
+
+let config = {
+      /** Setting the start point */
+      entry: './app/assets/scripts/App.js',
+      plugins:pages,
+      module:{
+        rules:[
+            cssConfig
+        ]
+    }
+}
+
+if(currentTask == 'dev'){
+    cssConfig.use.unshift('style-loader')
+    config.output = {
         filename:'bundled.js',
         path:path.resolve(__dirname,'app')
-    },
-    devServer: {
+    }
+    config.devServer ={
         before:function(app,server){
             server._watch('./app/**/*.html')
         },
@@ -28,16 +64,39 @@ module.exports = {
         port:3000,
         /**Can access site from an devices connected to the same network */
         host:'0.0.0.0'
-    },
-    mode:'development',
-
-    /**automatically rebiuild the dist folder after each save */
-     module:{
-        rules:[
-            {
-                test:/\.css$/i,
-                use:['style-loader', 'css-loader?url=false', {loader:'postcss-loader',options:{plugins:postCSSPlugins}}]
-            }
-        ]
     }
-}
+    config.mode ='development'
+}   
+
+
+if(currentTask == 'build'){
+    config.module.rules.push ({
+        test:/\.js$/,
+        exclude:/(node_modules)/,
+        use:{
+            loader:'babel-loader',
+            options:{
+                presets : ['@babel/preset-env']
+            }
+        }
+    })
+
+   cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+   postCSSPlugins.push(require('cssnano'))
+    config.output = {
+        filename:'[name].[chunkhash].js',
+        chunkFilename :'[name].[chunkhash].js',
+        path:path.resolve(__dirname,'docs')
+    }
+    config.mode ='production'
+    config.optimization = {
+        splitChunks :{chunks:'all'}
+    }
+   config.plugins.push(
+       new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({filename : 'styles.[chunkhash].css'}),
+      new RunAfterCompile()
+      )
+}  
+
+module.exports = config
